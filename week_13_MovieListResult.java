fabc25e90d2c14b143f19abf53d26083812a0da0
jungsarah <71162233+jungsarah@users.noreply.github.com>
2020-12-06 23:45:15 +0900

    Create week_13_MovieListResult.java

---
+++ week_13_MovieListResult.java
@@ -0,0 +1,11 @@
+package com.example.week12;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MovieListResult {
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
